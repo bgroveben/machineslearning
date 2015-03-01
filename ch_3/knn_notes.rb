9766e3a2086781aa92a596567907c1543ba454fc
bgrove1974 <bgrove1974@gmail.com>
2015-03-01 00:54:17 -0800

    House Happiness

---
+++ ch_3/knn_notes.rb
@@ -0,0 +1,3 @@
+# K-Nearest Neighbors classification is an instance-based supervised learning method that works well with distance-sensitive data.
+# It suffers from the curse of dimensionality and other problems with distance-based algorithms as we'll discuss.
+# Developed as a means of finding classifications among distributions that are unknown.
