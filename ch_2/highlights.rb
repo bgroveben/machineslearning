f0ed7e07e5043eddee96aede200fe36f612fd6ca
bgroveben <bgroveben@gmail.com>
2018-11-28 23:51:07 -0800

    Repurpose repository for a website on Artificial Intelligence

--- ch_2/highlights.rb
+++
@@ -1,3 +0,0 @@
-# This book will cover supervised and unsupervised machine learning methods.
-# Table 2-2 starting on pp17 provides a machine learning algorithm matrix.
-# Table 2-3 starting on pp19 provides a list of mathematical notations used in the book.
